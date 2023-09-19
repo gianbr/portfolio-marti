e17811fb32311b3e24c554ec74f7ac09a2f863f0
Gian Brunner <gianbrunner1@gmail.com>
2023-09-19 13:53:59 -0300

    added favicon, quick journey design fix, vercel 404 quickfix

--- src/Components/Journey/Journey.jsx
+++ src/Components/Journey/Journey.jsx
@@ -3,7 +3,7 @@ import journeyImg from '../../img/journey.png'
 
 export const Journey = () => {
   return (
-    <div className='flex items-center justify-center lg:max-w-3xl xl:max-w-4xl 2xl:max-w-7xl m-auto animate-fade animate-duration-[800ms]'>
+    <div className='flex items-center justify-center lg:max-w-3xl xl:max-w-4xl 2xl:max-w-[62.5rem] m-auto animate-fade animate-duration-[800ms]'>
         <img src={journeyImg} alt="Journey" />
     </div>
   )
